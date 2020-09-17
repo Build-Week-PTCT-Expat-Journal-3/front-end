03906a4d2cf1579e7bad39d87b8e1dec1324d743
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-09-17 18:47:23 -0500

    added profile to nav

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import {PostForm} from './PostForm';
-import {Posts} from './Posts'; 
+import {Posts} from './Posts';
 
 export const Dashboard = () => {
     return (
         <div>
             Dashboard
+
+            <Link to='/profile'>Go To Profile</Link>
 
             <PostForm />
             <Posts />
