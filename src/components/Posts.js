dbaab2ef078e90b1aee9b73875ece5600e7b71b6
Jason Rostar <jason.rostar@yahoo.com>
2020-09-22 17:45:23 -0700

    sample posts are showing in dashboard

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-export const Posts = () => {
+export const Posts = (props) => {
+    const { title, date, location, description, image_url } = props.post;
+
     return (
         <div>
-            Posts
+             <div >{title}</div>
+      <br></br>
+      <br></br>
+      <div >{date}</div>
+      <br></br>
+      <br></br>
+      <div >{location}</div>
+      <br></br>
+      <br></br>
+      <div >{description}</div>
+      <br></br>
+      <br></br>
+      <div >
+        <img src={image_url} alt="some random image that generated" />
+      </div>
         </div>
     )
 }
