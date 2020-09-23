60c0bd8247b45e079272b893b2adef6b0cc7f375
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-09-22 22:50:01 -0500

    edit-delete-buttons

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -20,7 +22,7 @@ const useStyles = makeStyles({
 export const Posts = (props) => {
   const { title, date, location, description, image_url } = props.post;
   const classes = useStyles();
-  
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -46,6 +48,14 @@ export const Posts = (props) => {
           </Typography>
         </CardContent>
       </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+        <Button size="small" color="primary">
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   )
 }
