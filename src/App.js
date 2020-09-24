13e38c2937cb60b4e255539b681b9611c5309618
Jason Rostar <jason.rostar@yahoo.com>
2020-09-23 17:45:45 -0700

    fixed posts loading error on log in

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         .catch((err) => console.log(err));
     };
     getData();
-  }, [setPosts]);
+  }, [setPosts, setLoggedState]);
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
